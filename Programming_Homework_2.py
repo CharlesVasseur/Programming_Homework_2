8cb34ae03be8d01eef870b2234b975dce8197738
CharlesVasseur <charles.vasseur@protonmail.com>
2026-01-29 20:53:28 +0100

    Fifth commit: I added exceptions, so that you have to try again when putting a word rather than a number.

--- Programming_Homework_2.py
+++ Programming_Homework_2.py
@@ -1,9 +1,21 @@
 # IF Problem: given the gross salary of a person, calculate the net
 
 gross = input("What is your gross salary? ")
-gross = int(gross)
+try:
+    gross = int(gross)
+except ValueError:
+    gross = input("Please enter a valid number: ")
+    gross = int(gross)
+
 child = input("To determine if you have an extra tax cut, how many children do you have? ")
-child = int(child)
+try:
+    child = int(child)
+except ValueError:
+    child = input("Please enter a valid number: ")
+    child = int(child)
+except int(child)<0:
+    child = input("This is not a valid number of children, try again: ")
+    child = int(child)
 
 if gross>4000:
     net = (0.82+0.005*child)*gross
