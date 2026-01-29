1289d3c156dbcf37403fcab395dc80271b0967e1
CharlesVasseur <charles.vasseur@protonmail.com>
2026-01-29 20:37:02 +0100

    Third commit: I have adapted the code to consider the number of children when calculating the taxes.

--- Programming_Homework_2.py
+++ Programming_Homework_2.py
@@ -2,17 +2,18 @@
 
 gross = input("What is your gross salary? ")
 gross = int(gross)
+child = input("To determine if you have an extra tax cut, how many children do you have? ")
+child = int(child)
 
 if gross>4000:
-    net = 0.82*gross
+    net = (0.82-0.005*child)*gross
     print(net)
 elif gross >= 2000:
-    net = 0.86*gross
+    net = (0.86-0.01*child)*gross
     print(net)
 elif gross >= 1000:
-    net = 0.88*gross
+    net = (0.88-0.01*child)*gross
     print(net)
 else:
-    net = 0.9*gross
+    net = (0.9-0.01*child)*gross
     print(net)
-
